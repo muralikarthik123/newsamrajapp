8292a28040a890bc0ec1a256058ec0429b7ba213
muralikarthik123 <160646378+muralikarthik123@users.noreply.github.com>
2024-12-02 14:51:11 +0530

    start.jsx package-lock.jsoo n package.json

--- src/start/start.jsx
+++ src/start/start.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import './start.css'
-import { FaArrowRight } from "react-icons/fa6";
+import { FaCoffee } from 'react-icons/fa';
+
 export default function home() {
   return (
     <div className='p1'>
       <img className='image1' src='./img1.jpeg' alt=' '/>
       <h1>Welcome to Samraj Solar Energies</h1>
       <p>We Specialize in delivering Both Large-scale and Small-Scale Solar projects,tralored to meet the unique needs of every client</p>
-      <button style={{display:'flex'}} className='bt1'>Get Started<span style={{marginLeft:'10px'}}><FaArrowRight/></span></button>
+      <button style={{display:'flex'}} className='bt1'>Get Started<span style={{marginLeft:'10px'}}><FaCoffee/></span></button>
     </div>
   )
 }
