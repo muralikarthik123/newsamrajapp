4bc8d3b3d13beb07d8da9d210acd105df2cc181b
muralikarthik123 <160646378+muralikarthik123@users.noreply.github.com>
2024-12-02 11:47:34 +0530

    first commit

---
+++ src/start/start.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './start.css'
+export default function home() {
+  return (
+    <div className='p1'>
+      <img className='image1' src='./img1.jpeg' alt=' '/>
+      <h1>Welcome to Samraj Solar Energies</h1>
+      <p>We Specialize in delivering Both Large-scale and Small-Scale Solar projects,tralored to meet the unique needs of every client</p>
+      <button className='bt1'>Get Started</button>
+    </div>
+  )
+}
