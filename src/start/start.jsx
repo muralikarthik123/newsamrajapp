5e61d6b13438b0419075ba27e9eae7cba0dc2f1c
muralikarthik123 <160646378+muralikarthik123@users.noreply.github.com>
2024-12-02 14:12:48 +0530

    start.css

--- src/start/start.jsx
+++ src/start/start.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import './start.css'
+import { FaArrowRight } from "react-icons/fa";
 export default function home() {
   return (
     <div className='p1'>
       <img className='image1' src='./img1.jpeg' alt=' '/>
       <h1>Welcome to Samraj Solar Energies</h1>
       <p>We Specialize in delivering Both Large-scale and Small-Scale Solar projects,tralored to meet the unique needs of every client</p>
-      <button className='bt1'>Get Started</button>
+      <button style={{display:'flex'}} className='bt1'>Get Started<span style={{marginLeft:'10px'}}><FaArrowRight/></span></button>
     </div>
   )
 }
