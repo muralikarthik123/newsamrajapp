93beb09ae90b042522b4169c8fbf61b61ef170b3
muralikarthik123 <160646378+muralikarthik123@users.noreply.github.com>
2024-12-03 21:37:30 +0530

    home.css home.jsx start.css

--- src/home/home.jsx
+++ src/home/home.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import './home.css'
-import { SlCalculator } from "react-icons/sl";
-import { CgSearchLoading } from "react-icons/cg";
+import { MdCalculate } from "react-icons/md";
+import { IoSearchCircleSharp } from "react-icons/io5";
 import { AiFillRobot } from "react-icons/ai";
-import { MdAppRegistration } from "react-icons/md";
+import { MdEditSquare } from "react-icons/md";
 import { HiChevronDoubleRight } from "react-icons/hi2";
 export default function home() {
   return (
     <div className='p2'>
-      <img className='img2' src='./img2.jpeg' alt=''/>
-      <h1>Features</h1>
+      <img className='img2' src='./img2.png' alt=''/>
+      <br/>
+      <h1>Explore</h1>
       <div style={{display:'flex'}}>
         <p className='more'>more</p>
         <HiChevronDoubleRight className='icon1'/>
@@ -17,13 +18,13 @@ export default function home() {
       <div className='c1'>
         {/*1 and 2 buttons*/}
             <div className='boxes'>
-            <button className='bt13'><SlCalculator className='icons'/></button>
+            <button className='bt13'>< MdCalculate className='icons'/></button>
             <br/>
             <p>Calucaltor</p>
             </div>
 
             <div className='boxes'>
-            <button className='bt13'><CgSearchLoading  className='icons'/></button>
+            <button className='bt13'><IoSearchCircleSharp className='icon2'/></button>
             <br/>
             <p>Subsidy</p>
             </div>
@@ -37,7 +38,7 @@ export default function home() {
           </div>
 
           <div className='boxes'>
-            <button className='bt13'><MdAppRegistration className='icons'/></button>
+            <button className='bt13'><MdEditSquare  className='icons'/></button>
             <br/>
             <p>Registration</p>
           </div>      
