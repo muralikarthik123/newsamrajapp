07acf8d3ed1ad8938840187135cd43a94e001ab9
muralikarthik123 <160646378+muralikarthik123@users.noreply.github.com>
2025-01-09 10:47:35 +0530

    data.json customerstatus.css customerstatus.jsx customer.css

--- src/customerstatus/customerstatus.jsx
+++ src/customerstatus/customerstatus.jsx
@@ -3,12 +3,17 @@ import './customerstatus.css'
 import Data from '../data.json'
 export default function customer() {
   return (
-    <div>
-      <p>Customer status</p>
+    <div className='status'>
+      <h1>Customer status</h1>
       {Data.map((item,index)=>(
-        <div key={index}>
-          <p>{item.name}</p>
-          <img src={item.image1} alt='e'/>
+        <div className='boxes' key={index}>
+          <p><p><b>User ID:</b>{item.id}</p></p>
+          <p><b>Name:</b>{item.name}</p>
+          <p><b>Site Servey:</b>{item.SiteServey}</p>
+          <p><b>Installation:</b>{item.Installation}</p>
+          <p><b>Documenation:</b>{item.Documenation}</p>
+          <p><b>SubsidyClamied:</b>{item.SubsidyClamied}</p>
+          <br/>
         </div>
       ))}
     </div>
