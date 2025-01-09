af54cdce75d1d790053ed9141e22bc98e5e015d4
muralikarthik123 <160646378+muralikarthik123@users.noreply.github.com>
2025-01-09 13:29:25 +0530

    customerstatus.css customerstatus.jsx

--- src/customerstatus/customerstatus.jsx
+++ src/customerstatus/customerstatus.jsx
@@ -1,30 +1,43 @@
-import React from 'react'
-import './customerstatus.css'
-import Data from '../data.json'
-export default function customer() {
+import React, { useState } from 'react';
+import './customerstatus.css';
+import Data from '../data.json';
+import { FaSearch } from "react-icons/fa";
+export default function Customer() {
+  const [search, setSearch] = useState('');
+
+  const open = (e) => {
+    setSearch(e.target.value);
+  };
+
   return (
     <div className='status'>
+      <div className='searchbox'>
+        <input
+        type='text'
+        placeholder='Enter the name or ID'
+        value={search}
+        onChange={open}
+        className='search'
+        />
+        <div className='icondis'>
+          <FaSearch className='searchicon'/>
+        </div>
+      </div>
       <h1>Customer status</h1>
-      {Data.map((item,index)=>(
+      {Data.filter((item) =>
+        (item.name && item.name.toLowerCase().includes(search.toLowerCase())) || //searching with name
+        (item.id && item.id.toString().includes(search)) // Search by ID as well
+      ).map((item, index) => (
         <div className='boxes' key={index}>
-          <p><p><b>User ID:</b>{item.id}</p></p>
-          <p><b>Name:</b>{item.name}</p>
-          <p><b>Site Servey:</b>{item.SiteServey}</p>
-          <p><b>Installation:</b>{item.Installation}</p>
-          <p><b>Documenation:</b>{item.Documenation}</p>
-          <p><b>SubsidyClamied:</b>{item.SubsidyClamied}</p>
-          <br/>
+          <p><b>User ID:</b> {item.id}</p>
+          <p><b>Name:</b> {item.name}</p>
+          <p><b>Site Survey:</b> {item.SiteServey}</p>
+          <p><b>Installation:</b> {item.Installation}</p>
+          <p><b>Documentation:</b> {item.Documenation}</p>
+          <p><b>Subsidy Claimed:</b> {item.SubsidyClamied}</p>
+          <br />
         </div>
       ))}
     </div>
-  )
+  );
 }
-
-
-
-
-
-  
-
-
-      
